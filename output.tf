9113abd080da531598e02e6412c9d9aa4eaa25ac
Tomaz Rocha <tmz_rocha@hotmail.com>
2025-11-26 01:32:35 +0000

    Alterando ID de subnets em Outputs

--- output.tf
+++ output.tf
@@ -4,21 +4,21 @@ output "vpc_id" {
 }
 
 output "subnet_public_1a" {
-  description = "Id da Public Subnet 1a"
+  description = "subnet-0d91501cca82fba5b"
   value       = aws_subnet.public_subnet_1a.id
 }
 
 output "subnet_private_1a" {
-  description = "Id da Private Subnet 1a"
+  description = "subnet-08657af497270c583"
   value       = aws_subnet.private_subnet_1a.id
 }
 
 output "subnet_public_1b" {
-  description = "Id da Public Subnet 1b"
+  description = "subnet-09e34edc0c30c2c83"
   value       = aws_subnet.public_subnet_1b.id
 }
 
 output "subnet_private_1b" {
-  description = "Id da Private Subnet 1b"
+  description = "subnet-0a8e6a6a7c0b6fe98"
   value       = aws_subnet.private_subnet_1b.id
 }
